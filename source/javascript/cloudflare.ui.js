5a44e0e0d1ea2a497b37d15c639ef17118d23f2b
Christopher Joel <chris@cloudflare.com>
2010-10-18 14:25:54 -0700

    Okay, got the preliminary jakefile in there.. gotta make it dosomething useful

---
+++ source/javascript/cloudflare.ui.js
@@ -0,0 +1,11 @@
+/* @depends 'jquery/jquery-1.4.3.js'
+ * @depends 'cloudflare/cloudflare.ui.core.js'
+ * @depends 'cloudflare/cloudflare.ui.gesture.js'
+ * @depends 'cloudflare/cloudflare.ui.component.js'
+ * @depends 'cloudflare/cloudflare.ui.datarenderer.js'
+ * @depends 'cloudflare/cloudflare.ui.button.js'
+ * @depends 'cloudflare/cloudflare.ui.list.js'
+ *
+ * Note: this file exists to provide Juicer with an order of dependencies for
+ * when it combines the library. DO NOT INCLUDE THIS FILE DIRECTLY ON A PAGE!
+ */
